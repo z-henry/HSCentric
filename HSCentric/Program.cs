b4600f3cf63c56c561131a506ef65f4db4805fbd
hy z <henry_zzzzz@outlook.com>
2022-07-06 15:45:37 +0800

    加入dump分析

--- HSCentric/Program.cs
+++ HSCentric/Program.cs
@@ -10,9 +10,21 @@ namespace HSCentric
 		[STAThread]
 		private static void Main()
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new MainForm());
+			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler((obj, args) => MiniDump.Write());
+			try
+			{
+				Application.EnableVisualStyles();
+				Application.SetCompatibleTextRenderingDefault(false);
+				Application.Run(new MainForm());
+			}
+
+			catch (Exception ex)
+			{
+				Out.Log("空间名：" + ex.Source + "；" + '\n' +
+					"方法名：" + ex.TargetSite + '\n' +
+					"故障点：" + ex.StackTrace.Substring(ex.StackTrace.LastIndexOf("\\") + 1, ex.StackTrace.Length - ex.StackTrace.LastIndexOf("\\") - 1) + '\n' +
+					"错误提示：" + ex.Message);
+			}
 		}
 	}
 }
