817721cc06697f52f838fb05ba23ae5c304b74fd
hy z <henry_zzzzz@outlook.com>
2022-10-07 23:23:37 +0800

    修复

--- HSCentric/WebRestFul.cs
+++ HSCentric/WebRestFul.cs
@@ -14,17 +14,17 @@ namespace HSCentric
 		//说明：GET请求
 		//WebGet默认请求是GET方式
 		//UriTemplate(URL Routing)input必须要方法的参数名必须一致不区分大小写）
-		//curl "http://124.221.7.52:9000/hscentric/v1/Hearthstone" -X GET
-// 		[OperationContract]
-// 		[WebGet(UriTemplate = "hscentric/v1/{input}", BodyStyle = WebMessageBodyStyle.Bare, ResponseFormat = WebMessageFormat.Json)]
-// 		ResultInfo GetInputFunc(string input);
+		//curl "http://127.0.0.1:9000/hscentric/v1/Hearthstone" -X GET
+		// 		[OperationContract]
+		// 		[WebGet(UriTemplate = "hscentric/v1/{input}", BodyStyle = WebMessageBodyStyle.Bare, ResponseFormat = WebMessageFormat.Json)]
+		// 		ResultInfo GetInputFunc(string input);
 
 		//说明：POST请求
 		//WebInvoke请求方式有POST、PUT、DELETE等，所以需要明确指定Method是哪种请求的，这里我们设置POST请求。
 		//UriTemplate(URL Routing)input必须要方法的参数名必须一致不区分大小写）
 		//RequestFormat规定客户端必须是什么数据格式请求的（JSon或者XML），不设置默认为XML
 		// ResponseFormat规定服务端返回给客户端是以是什么数据格返回的（JSon或者XML）
-		//>curl "http://124.221.7.52:9000/hscentric/v1" -H "Content-Type:application/json" -d "{\"enable\":false,\"name\":\"Hearthstone\"}" -X POST
+		//>curl "http://127.0.0.1:9000/hscentric/v1" -H "Content-Type:application/json" -d "{\"enable\":false,\"name\":\"Hearthstone\"}" -X POST
 		[OperationContract]
 		[WebInvoke(Method = "POST", UriTemplate = "hscentric", BodyStyle = WebMessageBodyStyle.Bare, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
 		ResultInfo PostInputFunc(RequestInfo requestInfo);
