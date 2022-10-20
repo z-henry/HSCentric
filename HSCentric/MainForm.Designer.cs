ef8efb905a8abc121366e13cd92e2cdd6d9385ea
hy z <henry_zzzzz@outlook.com>
2022-10-20 17:26:54 +0800

    任务名称调整适配

--- HSCentric/MainForm.Designer.cs
+++ HSCentric/MainForm.Designer.cs
@@ -198,7 +198,7 @@ namespace HSCentric
 			this.Controls.Add(this.btn_del);
 			this.Controls.Add(this.btn_add);
 			this.Name = "MainForm";
-			this.Text = "中控 5.1.8";
+			this.Text = "中控 5.1.9";
 			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
 			this.contextMenuStrip_RMenu.ResumeLayout(false);
 			this.ResumeLayout(false);
